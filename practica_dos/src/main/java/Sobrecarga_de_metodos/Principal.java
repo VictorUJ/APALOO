826c7452c4a4d29a6841d8eb6e6280525b46ae83
Victor Uribe <v.uj@hotmail.com>
2021-01-05 14:00:34 -0600

    Respuestas para Herencia 3

--- practica_dos/src/main/java/Sobrecarga_de_metodos/Principal.java
+++ practica_dos/src/main/java/Sobrecarga_de_metodos/Principal.java
@@ -12,7 +12,17 @@ package Sobrecarga_de_metodos;
 public class Principal {
 
     /**
-     * @param args the command line arguments
+     * 1. Existen un error en el llamado al no haber un método sobrecargado que reciba un
+           entero?
+           Repuesta: No ya que existe un metodo que recibe double y puede convertir el entero a double
+       2. Si es que no hay error, que método sobrecargado se ejecuta?
+       *   Repuesta: void test(double a){}
+       3. Si se invierten los tpos de datos que pasa?
+▪         void test(double a) ----> void test(int a)
+▪         int i ------> double i
+*         Repuesta: Saldria un error ya que se pierde informacion al convertir del double a int
+*      4. De lo anterior, que diferencias hay (o no hay) con la sobrecarga de constructores?
+*         Repuesta: La unica diferencia esta al utilizar el llamado super en caso de definir un caso para algun constructor
      */
     public static void main(String[] args) {
         // TODO code application logic here
